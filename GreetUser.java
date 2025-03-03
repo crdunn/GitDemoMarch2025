03423b2664d2bafb2a84debc76594c895d1412e0
Charles Dunn <charles.dunn@ascendion.com>
2025-03-03 13:47:32 -0500

    missing a semicolon.  How embarassing.

--- GreetUser.java
+++ GreetUser.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class GreetUser {
 	public static greet(Scanner scan){
-		System.out.println("Enter your name: ")
+		System.out.println("Enter your name: ");
 		String user = scan.nextLine();
 		System.out.println("Hello " + user); 
 	}
